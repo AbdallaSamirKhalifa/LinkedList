ae5376cd8d2d96b77d5c020d383130c6ec30cdaf
AbdallaSamirKhalifa <abdallasamirkhalifa@gmail.com>
2025-04-22 22:08:05 +0200

    initial commit

---
+++ Node.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+
+
+
+internal class Node<T>
+{
+    public Node<T> Next {  get; set; }
+    public T Data { get; set; }
+    
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+
+
+
+}
+
+internal class SinglyLinkedList<T>
+{
+    private Node<T> _head;
+
+    public int Count { get; private set; }
+    public void AddFirst(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        newNode.Next = _head;
+        _head = newNode;
+        Count++;
+    }
+
+    public void AddLast(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+
+        if(_head == null)
+        {
+            _head = newNode;
+            Count++;
+            return;
+        }
+
+        Node<T> current = _head;
+        while(current.Next != null)
+        {
+            current = current.Next;
+
+
+        }
+        current.Next = newNode;
+        Count++;
+    }
+
+    public void AddLastOptimized(T data)
+    {
+        if (_head.Next == null)
+        {
+            AddAfter(_head, data);
+
+            return;
+        }
+
+        Node<T> current = _head;
+        while (current.Next != null)
+        {
+            current = current.Next;
+        }
+        AddAfter(current, data);
+    }
+
+    public bool Remove(T data)
+    {
+        if (_head == null)
+            return false;
+
+        Node<T> current=_head ,prev = _head;
+        if (current.Data.Equals(data))
+        {
+            current = current.Next;
+            Count--;
+            return true;
+        }
+
+        while(current != null)
+        {
+            if (current.Data.Equals(data))
+            {
+                prev.Next = current.Next;
+                Count--;
+                return true;
+
+            }
+            prev = current;
+            current = current.Next;
+        }
+
+        return false; 
+
+    }
+
+    public bool RemoveFirst()
+    {
+        if(_head == null)
+            return false;
+        _head= _head.Next;
+        Count--;
+        return true;
+    }
+
+    public bool RemoveLast()
+    {
+        if (_head == null)
+            return false;
+
+        Node<T> lastNode = _head, prev = _head;
+
+        while(lastNode.Next!=null)
+        {
+            prev = lastNode;
+            lastNode = lastNode.Next;
+        }
+        prev.Next = null;
+        Count--;
+        return true;
+
+    }
+    public void AddAfter(Node<T> prev,T data)
+    {
+        if(prev == null)
+            return;
+
+        Node<T> newNode = new Node<T>(data);
+
+        newNode.Next = prev.Next;
+        prev.Next = newNode;
+        Count++;
+        
+    }
+
+    public Node<T> Find(T data)
+    {
+
+        Node<T> current = _head;
+        while(current != null)
+        {
+            if(current.Data.Equals(data))
+                return current;
+
+            current = current.Next;
+        }
+        return null;
+    }
+
+    public void PrintList()
+    {
+
+        Node<T> current = _head;
+        while (current != null)
+        {
+            
+            Console.Write($"{current.Data} -> ");
+            current=current.Next;
+        }
+
+        Console.Write("Null");
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+
+        list.AddLast(1);
+        list.AddLast(2);
+        //list.AddLast(3);
+        list.AddFirst(0);
+
+        Console.WriteLine("Forward traversal:");
+        list.PrintForward();
+        Console.WriteLine(list.Count);
+        Console.WriteLine("\n\nBackward traversal:");
+        list.PrintBackward();
+        Console.WriteLine(list.Count);
+
+        //list.AddAfter(list.Last, 500);
+
+        Console.WriteLine("\n\nAfter inserting:");
+        list.PrintForward();
+        list.Reverse();
+
+        list.PrintForward();
+
+        Console.ReadKey();
+    }
+}
+
